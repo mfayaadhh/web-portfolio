cb16967b93c8a8aeb73fdc312601dff3b6f0576b
Muhammad Fayaadh <fayaadh93@gmail.com>
2025-08-24 09:00:44 +0700

    upd section baru

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,9 @@ export default function Home() {
             />
           </div>
           <div className="w-1/2">
-            <h2 className="text-4xl mb-10 font-bold">Solusi Website untuk Bisnis Anda</h2>
+            <h2 className="text-4xl mb-10 font-bold">
+              Solusi Website untuk Bisnis Anda
+            </h2>
             <p className="text-xl">
               Saya menyediakan layanan pembuatan website yang simpel, cepat, dan
               terjangkau. Website yang saya buat tidak hanya terlihat
@@ -73,35 +75,150 @@ export default function Home() {
         </div>
       </section>
       <section className="p-10">
-        <h2 className="text-4xl p-2.5 text-center">Hasil Karya Saya</h2>
+        <h2 className="text-4xl p-2.5 text-center font-bold">
+          Hasil Karya Saya
+        </h2>
         {/* Anak Section */}
         <div className="flex p-10">
-          <div className="w-1/2 ">
-            <p className="text-xl">
-              Setiap website yang saya buat adalah hasil dari perhatian terhadap
-              detail, keindahan desain, dan kebutuhan bisnis klien. Di sini Anda
-              bisa melihat beberapa proyek yang telah saya kerjakan, mulai dari
-              website bisnis kecil hingga website personal yang modern dan
-              fungsional.
-            </p>
-          </div>
-          <div className="card bg-base-100 w-1/2 shadow-sm">
-            <figure>
-              <img
-                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                alt="Shoes"
-              />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Project Babibu</h2>
-              <p>
-                A card component has a figure, a body part, and inside body
-                there are title and actions parts
+          <div className="flex w-full">
+            <div className="w-1/2 ">
+              <p className="text-xl">
+                Setiap website yang saya buat adalah hasil dari perhatian
+                terhadap detail, keindahan desain, dan kebutuhan bisnis klien.
+                Di sini Anda bisa melihat beberapa proyek yang telah saya
+                kerjakan, mulai dari website bisnis kecil hingga website
+                personal yang modern dan fungsional.
               </p>
-              <div className="card-actions justify-center">
-                <button className="btn btn-primary btn-outline">
-                  Cek Sekarang
-                </button>
+            </div>
+            <div className="divider divider-horizontal"></div>
+            <div className="card bg-base-100 w-1/2 shadow-sm">
+              <figure>
+                <img
+                  src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Project Babibu</h2>
+                <p>
+                  A card component has a figure, a body part, and inside body
+                  there are title and actions parts
+                </p>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-outline mt-5">
+                    Cek Sekarang
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="p-10 bg-base-300 ">
+        <h2 className="text-4xl p-2.5 text-center font-bold">
+          Keunggulan Website Saya
+        </h2>
+        {/* Anak Section */}
+        <div>
+          {/* Baris 1*/}
+          <div className="flex justify-between p-5">
+            {/* Card 1 */}
+            <div className="card bg-base-100 w-96 shadow-sm m-3">
+              <figure>
+                <img
+                  src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title sm:text-sm">Mudah Digunakan</h2>
+                <p>Website nyaman dan gampang dipahami oleh siapa saja.</p>
+              </div>
+            </div>
+            {/* Card 2 */}
+            <div className="card bg-base-100 w-96 shadow-sm m-3">
+              <figure>
+                <img
+                  src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title sm:text-sm">
+                  Tampil Bagus di HP & Laptop
+                </h2>
+                <p>
+                  Responsif di semua perangkat, dari smartphone sampai komputer.
+                </p>
+              </div>
+            </div>
+            {/* Card 3 */}
+            <div className="card bg-base-100 w-96 shadow-sm m-3">
+              <figure>
+                <img
+                  src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title sm:text-sm">
+                  Desain Menarik & Profesional
+                </h2>
+                <p>
+                  Cocok dengan identitas bisnis Anda, enak dipandang, dan
+                  membuat pelanggan betah.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Baris 2 */}
+          <div className="flex justify-between p-5">
+            {/* Card 1 */}
+            <div className="card bg-base-100 w-96 shadow-sm m-3">
+              <figure>
+                <img
+                  src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title sm:text-sm">Mudah Digunakan</h2>
+                <p>Website nyaman dan gampang dipahami oleh siapa saja.</p>
+              </div>
+            </div>
+            {/* Card 2 */}
+            <div className="card bg-base-100 w-96 shadow-sm m-3">
+              <figure>
+                <img
+                  src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title sm:text-sm">
+                  Tampil Bagus di HP & Laptop
+                </h2>
+                <p>
+                  Responsif di semua perangkat, dari smartphone sampai komputer.
+                </p>
+              </div>
+            </div>
+            {/* Card 3 */}
+            <div className="card bg-base-100 w-96 shadow-sm m-3">
+              <figure>
+                <img
+                  src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                  alt="Shoes"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title sm:text-sm">
+                  Desain Menarik & Profesional
+                </h2>
+                <p>
+                  Cocok dengan identitas bisnis Anda, enak dipandang, dan
+                  membuat pelanggan betah.
+                </p>
               </div>
             </div>
           </div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>projects page</div>
+    <div className="flex-grow">projects page</div>
   )
 }
